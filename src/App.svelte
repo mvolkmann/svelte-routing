d14d30bfcd12964d1065f4fbab26246c9dbaa5ed
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-12-18 11:43:23 -0600

    made this a more realistic example

--- src/App.svelte
+++ src/App.svelte
@@ -1,102 +1,63 @@
 <script>
   import page from 'page';
-  import qs from 'query-string';
-
-  // This is the "not found" page component.
-  import NotFound from './NotFound.svelte';
+  import {cartStore} from './stores.js';
 
   // These are the page components.
-  import Page1 from './Page1.svelte';
-  import Page2 from './Page2.svelte';
-  import Page3 from './Page3.svelte';
+  import Cart from './Cart.svelte';
+  import NotFound from './NotFound.svelte';
+  import Ship from './Ship.svelte';
+  import Shop from './Shop.svelte';
 
   // This holds the page component to be rendered.
   let component;
 
-  // This holds all the props to be passed to the page component.
-  let props = {};
-
-  // This is a middleware that parses query strings
-  // and puts the result back on the context object.
-  function parseQueryString(context, next) {
-    context.query = qs.parse(context.querystring);
-    props = {}; // clears previous value
-    next(); // allows next middleware to run
-  }
-
-  // This causes the parseQueryString middleware
-  // to run on every path.
-  // If the first parameter, '*', is omitted,
-  // it will do the same thing.
-  page('*', parseQueryString);
-
-  // This is the root path for the app.
-  page('/', context => {
-    component = Page1;
-    props = {p1: 'alpha', q1: 'beta'};
-  });
-
-  // This path requires one path parameter, p1.
-  // A second path parameter, p2, is optional.
-  // This is indicated by following the
-  // path parameter name with a question mark.
-  page('/one/:p1/:p2?', context => {
-    component = Page1;
-    const {params, query} = context;
-    // These are passed to the component to be rendered
-    // in <svelte:component> later.
-    props = {...params, ...query};
-  });
-
-  // This path doesn't use any path or query parameters.
-  page('/two', () => component = Page2);
-
-  // This path doesn't use any path or query parameters.
-  page('/three', () => component = Page3);
+  page.redirect('/', '/shop');
+  page('/cart', () => (component = Cart));
+  page('/ship', () => (component = Ship));
+  page('/shop', () => (component = Shop));
 
   // Since this is the last registered path
-  // and it matches everything
-  // it will be invoked for any path not already handled.
+  // and it matches everything.
+  // It will be invoked for any path not already handled.
   // If the first parameter, '*', is omitted,
   // it will do the same thing.
-  page('*', context => {
-    console.log('App.svelte x: context =', context);
-    component = NotFound;
-    props = {path: context.path};
-  });
+  page('*', () => (component = NotFound));
 
   // This tells "page" to start
   page.start();
 </script>
 
+<nav>
+  <a href="/shop" class:active={component === Shop}>Shop</a>
+  <a class="icon" href="/cart" class:active={component === Cart}>&#x1F6D2; {$cartStore.length}</a>
+  <a href="/ship" class:active={component === Ship}>Ship</a>
+</nav>
+
+<main>
+  <svelte:component this={component} />
+</main>
+
 <style>
-  :global(body) {
-    padding: 0;
-  }
-
-  :global(h1) {
-    margin-top: 0;
-  }
-
+  /* This defines a CSS variable that any rule can use. */
   :root {
-    --space: 20px;
+    --space: 10px;
   }
 
   a {
-    --padding: 5px;
     background-color: white;
-    border: solid gray 1px;
-    border-radius: var(--padding);
-    color: black;
-    display: inline-block;
-    margin-right: calc(var(--space) / 2);
-    padding: var(--padding);
+    border-radius: var(--space);
+    margin-right: var(--space);
+    padding: var(--space);
     text-decoration: none;
   }
 
-  button {
-    margin-bottom: 0;
-    margin-top: var(--space);
+  a.active {
+    background-color: yellow;
+  }
+
+  .icon {
+    padding-bottom: 6px;
+    padding-top: 6px;
   }
 
   main {
@@ -104,22 +65,9 @@
   }
 
   nav {
+    display: flex;
+    align-items: center;
     background-color: cornflowerblue;
     padding: var(--space);
   }
 </style>
-
-<nav>
-  <!-- Note how this link uses different path
-       and query parameters than the "/" path. -->
-  <a href="/one/v1/v2?q1=v3&q2=v4">One</a>
-  <a href="/two">Two</a>
-  <a href="/three">Three</a>
-</nav>
-
-<main>
-  <svelte:component this={component} {...props} />
-
-  <!-- This demonstrates programmatic navigation. -->
-  <button on:click={() => page.show('/two')}>Go To Page Two</button>
-</main>
--- src/NotFound.svelte
+++ src/NotFound.svelte
@@ -1,7 +1,1 @@
-<script>
-  export let path;
-</script>
-
 <h1>Page Not Found</h1>
-
-<div>The path was {path}.</div>
