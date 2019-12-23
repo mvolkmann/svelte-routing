bbc0fa963e432294fd3148e73539977a3b1d0c7c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-12-23 11:00:55 -0600

    changes in sveltejs/template have broken page routing

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
-import rollup_start_dev from './rollup_start_dev';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -13,7 +12,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/bundle.js'
+		file: 'public/build/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -22,7 +21,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/bundle.css');
+				css.write('public/build/bundle.css');
 			}
 		}),
 
@@ -37,9 +36,9 @@ export default {
 		}),
 		commonjs(),
 
-		// In dev mode, call `npm run start:dev` once
+		// In dev mode, call `npm run start` once
 		// the bundle has been generated
-		!production && rollup_start_dev,
+		!production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
@@ -53,3 +52,20 @@ export default {
 		clearScreen: false
 	}
 };
+
+function serve() {
+	let started = false;
+
+	return {
+		writeBundle() {
+			if (!started) {
+				started = true;
+
+				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+					stdio: ['ignore', 'inherit', 'inherit'],
+					shell: true
+				});
+			}
+		}
+	};
+}
