9741a81509292b088384ea5f2b9824f04870dce5
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-12-24 09:03:05 -0600

    changed to use hash routing instead of page library

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-  import page from 'page';
   import {cartStore} from './stores.js';
 
   // These are the page components.
@@ -8,29 +7,24 @@
   import Ship from './Ship.svelte';
   import Shop from './Shop.svelte';
 
+  const hashMap = {
+    '#cart': Cart,
+    '#ship': Ship,
+    '#shop': Shop
+  };
+
   // This holds the page component to be rendered.
-  let component;
+  let component = Shop;
 
-  page.redirect('/', '/shop');
-  page('/cart', () => (component = Cart));
-  page('/ship', () => (component = Ship));
-  page('/shop', () => (component = Shop));
-
-  // Since this is the last registered path
-  // and it matches everything.
-  // It will be invoked for any path not already handled.
-  // If the first parameter, '*', is omitted,
-  // it will do the same thing.
-  page('*', () => (component = NotFound));
-
-  // This tells "page" to start
-  page.start();
+  const hashChange = () => component = hashMap[location.hash] || NotFound;
 </script>
 
+<svelte:window on:hashchange={hashChange} />
+
 <nav>
-  <a href="/shop" class:active={component === Shop}>Shop</a>
-  <a class="icon" href="/cart" class:active={component === Cart}>&#x1F6D2; {$cartStore.length}</a>
-  <a href="/ship" class:active={component === Ship}>Ship</a>
+  <a href="/#shop" class:active={component === Shop}>Shop</a>
+  <a href="/#cart" class:active={component === Cart} class="icon">&#x1F6D2; {$cartStore.length}</a>
+  <a href="/#ship" class:active={component === Ship}>Ship</a>
 </nav>
 
 <main>
